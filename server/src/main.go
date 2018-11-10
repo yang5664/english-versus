2309cd55beaf5aa2c90a13560a5cbe1bdb893d88
Harland <>
2018-11-10 15:14:02 +0800

    testControl send action to server, server broadcast

--- server/src/main.go
+++ server/src/main.go
@@ -73,7 +73,9 @@ func handleMessages() {
 	for {
 		// Grab the next message from the broadcast channel
 		msg := <-broadcast
-		log.Println(msg)
+		log.Println(msg.Type)
+		log.Println(msg.Channel)
+		log.Println(msg.Message)
 		// Send it out to every client that is currently connected
 		for client := range clients {
 			err := client.WriteJSON(msg)
